76856f8ce8b09a699d79150f46e9639526681dd5
sofiane chaoui <sofiane.chaoui@vestalis.fr>
2024-02-20 15:09:54 +0100

    (bdd)[mysql] add new bdd

--- spring-boot-server/launcher.sh
+++ spring-boot-server/launcher.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-// remove jdbc:mysql:// from the url
 DATABASE_URI=$(echo $SPRING_DATASOURCE_URL | cut -c 14-)
 DATABASE_HOST=$(echo $DATABASE_URI | cut -d ':' -f 1)
 until mysqladmin ping -h "$DATABASE_HOST" --silent; do
